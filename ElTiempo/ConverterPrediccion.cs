10f1d1563051317c5d607ae50d56f528472bc674
Sergio Ramos Santonja <sergioramos55@outlook.es>
2019-11-20 14:10:06 +0100

    Agregar archivos de proyecto.

---
+++ ElTiempo/ConverterPrediccion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ElTiempo
+{
+    class ConverterPrediccion : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((string)value == "sol")
+                value = "sunny.png";
+            else if ((string)value == "nubes")
+                value = "cloudy.png";
+            else if ((string)value == "lluvia")
+                value = "rain.png";
+            else
+                value = "snow.png";
+
+            return value;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ElTiempo/ConverterTiempo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ElTiempo
+{
+    class ConverterTiempo : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int celsium = (int)(((int)value - 32) / 1.8);
+
+            string tiempo = celsium + "°";
+
+            return tiempo;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
